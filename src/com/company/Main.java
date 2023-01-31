fecfe33e7b43fc09b855b7a252d0a632967a3e3c
David P Semchishin 12th CCHS <080917@SWCSD.US>
2023-01-31 14:27:58 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        double temp = collectInput();
+        double conversion = conversion(temp);
+        display(conversion);
+
+    }
+
+
+
+
+    static double collectInput(){
+        System.out.println("Please enter the temp in fahrenheit: ");
+        double temp = input.nextDouble();
+        return temp;
+    }
+
+    static double conversion(double temp){
+        double celsius = (((temp - 32) * 5)/9);
+        return celsius;
+    }
+
+    static void display(double w){
+        System.out.println("the degrees in celsius is " + w);
+    }
+}
