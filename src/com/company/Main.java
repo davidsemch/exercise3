4b6136c0efddff49073c73cc22c58e98c7d91ee4
davidsemch <117207839+davidsemch@users.noreply.github.com>
2023-02-02 13:24:48 -0500

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,15 +5,15 @@ public class Main {
     static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-
         double temp = collectInput();
-        double conversion = conversion(temp);
-        display(conversion);
+
+        while (temp >= -460) {
+            double conversion = conversion(temp);
+            display(conversion);
+            temp = collectInput();
+        }
 
     }
-
-
-
 
     static double collectInput(){
         System.out.println("Please enter the temp in fahrenheit: ");
